3c1d3e24ed6119c8b5b5e3a7f0ae60af6d30c2c4
frking69 <121990793+frking69@users.noreply.github.com>
2024-03-10 19:16:26 +0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,19 @@
+public class Node {
+	 private int data;
+	    private Node next;
+
+	    public Node(int data) {
+	        this.data = data;
+	    }
+
+	    public void setNext(Node next) {
+	        this.next = next;
+	    }
+
+	    public int getdata() {
+	        return data;
+	    }
+
+	    public Node getNext() {
+	        return next;
+	    }
